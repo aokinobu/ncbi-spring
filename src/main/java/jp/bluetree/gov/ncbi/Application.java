7b08f502e0157e4c549bb5d29f254fb4b74f9ffb
aoki <aokinobu@gmail.com>
2016-11-22 15:36:45 +0900

    stop executable jar

--- src/main/java/jp/bluetree/gov/ncbi/Application.java
+++ src/main/java/jp/bluetree/gov/ncbi/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
